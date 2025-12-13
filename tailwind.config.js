57a094ffc852816c116d4a24090d3a54c6156dd1
aqgulsharipkhan-creator <your_github_email@example.com>
2025-12-13 12:11:27 +0500

    fix: nuxt tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: { extend: {} },
   plugins: [],
 }
-
